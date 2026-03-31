1343cc159a3ab3115e1e22121672f2f989396d39
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2026-03-31 11:01:01 +0100

    finishing exercise 0

--- py_module04/ex0/ft_ancient_text.py
+++ py_module04/ex0/ft_ancient_text.py
@@ -1,22 +1,28 @@
-def read_file(filename: str) -> None:
+def reading_file(filename: str) -> None:
+    f = None
     try:
         f = open(filename, "r")
         print("Accessing Storage Vault: " + filename)
         print("Connection established...\n")
         print("RECOVERED DATA:")
+
         content = f.read()
         print(content, "\n")
+
         print("Data recovery complete. Storage unit disconnected.")
+
     except FileNotFoundError:
         print("ERROR: Storage vault not found. Run data generator first.")
     except PermissionError:
         print("ERROR: Permission denied.")
     except Exception as err:
-        print(f"ERORR: {err.args}")
+        print(f"ERROR: {err}")
+
     finally:
-        f.close()
+        if f is not None:
+            f.close()
 
 
 if __name__ == "__main__":
     print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
-    read_file("ancient_fragment.txt")
+    reading_file("ancient_fragment.txt")
